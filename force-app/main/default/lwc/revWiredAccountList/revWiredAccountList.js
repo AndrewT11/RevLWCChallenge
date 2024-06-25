5a65155579e651043e13b90264b713fa0d6025f9
Andrew Tran <tranandrew.tech@gmail.com>
2024-06-25 12:08:06 -0500

    Account List Wired service completed and tested. Starting imperative apex challenge

---
+++ force-app/main/default/lwc/revWiredAccountList/revWiredAccountList.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire } from 'lwc';
+import getSearchAccounts from '@salesforce/apex/AccountHelper.getSearchAccounts';
+
+export default class RevWiredAccountList extends LightningElement {
+
+    searchedAccounts;
+    error;
+    searchWord;
+
+    // Wire service 
+    @wire(getSearchAccounts, {searchWord: '$searchWord'})
+    searchedAccounts;
+
+    columns = [
+        {label: 'Name', fieldName: 'Name', type: 'name'}
+    ];
+
+    handleSearch() {
+        let search = this.template.querySelector('.input').value;
+        this.searchWord = search;
+    }
+
+//     handleOnChange(event) {
+//         this.searchhWord = event.target.value;
+//     }
+}
